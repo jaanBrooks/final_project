6d074e4aaca4d2935446c16a82a060e6a7fceb5e
Jaan Brooks <jpbrooks@colgate.edu>
2026-04-15 13:12:16 -0400

    refactor(Global Constants) put constants in settings.py file

--- final_project.py
+++ final_project.py
@@ -2,19 +2,10 @@ import random
 import math
 from raylib import *
 from pyray import *
+from settings.py import *
 
 # --- Game Constants ---
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-TILE_SIZE = 40          # Size of one tile in pixels
-GRAVITY = 1800.0        # Downward acceleration (pixels/s/s)
-JUMP_VELOCITY = -750.0  # Initial upward velocity on jump
-STOMP_BOUNCE = JUMP_VELOCITY * 0.6 # Reduced jump velocity for bounce
-PLAYER_SPEED = 300.0    # Player horizontal movement speed
-ENEMY_SPEED = 100.0     # Enemy horizontal movement speed
-PLAYER_WIDTH = TILE_SIZE * 0.8
-PLAYER_HEIGHT = TILE_SIZE * 0.9
-BULLET_SPEED = 500.0 
+
 
 # --- Tilemap Definitions ---
 TILE_AIR = 0
---
+++ settings.py
@@ -0,0 +1,11 @@
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+TILE_SIZE = 40          # Size of one tile in pixels
+GRAVITY = 1800.0        # Downward acceleration (pixels/s/s)
+JUMP_VELOCITY = -750.0  # Initial upward velocity on jump
+STOMP_BOUNCE = JUMP_VELOCITY * 0.6 # Reduced jump velocity for bounce
+PLAYER_SPEED = 300.0    # Player horizontal movement speed
+ENEMY_SPEED = 100.0     # Enemy horizontal movement speed
+PLAYER_WIDTH = TILE_SIZE * 0.8
+PLAYER_HEIGHT = TILE_SIZE * 0.9
+BULLET_SPEED = 500.0 
